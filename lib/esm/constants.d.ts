313eb544d86b3da6046bb280d9bdf06b21e7cb9a
Jonathan Cobb <jonathan@kyuss.org>
2023-08-15 17:56:45 -0400

    add nap utility

--- lib/esm/constants.d.ts
+++ lib/esm/constants.d.ts
@@ -1,3 +1,8 @@
 import { MobilettoClock } from "./types.js";
 export declare const DEFAULT_CLOCK: MobilettoClock;
 export declare const sleep: (ms: number) => Promise<unknown>;
+export declare const DEFAULT_NAP_CHECK = 1000;
+export type NapAlarm = {
+    wake?: boolean;
+};
+export declare const nap: (clock: MobilettoClock, alarm: NapAlarm, ms: number, check?: number) => Promise<unknown>;
